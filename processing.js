8b884c590787dc9c15690000db883a37994e0125
Francisco Javier Sánchez <tragadiscos@gmail.com>
2019-12-19 00:57:26 +0100

    Add Number of Queries

--- processing.js
+++ processing.js
@@ -15,8 +15,6 @@ function handleSampleDataQueryResponse(response) {
         return;
     }
 
-    // Sets chart options.
-
     var colors = ['#a6cee3', '#b2df8a', '#fb9a99', '#fdbf6f', '#cab2d6', '#ffff99', '#1f78b4', '#33a02c'];
     var piecolors = ['#F66D44','#FEAE65','#E6F69D','#AADEA7','#64C2A6','#2D87BB','#E0E0E0'];
     var options = {
@@ -24,7 +22,7 @@ function handleSampleDataQueryResponse(response) {
         height: 600,
         sankey: {
             link: {
-                colorMode: 'source', // 'gradient'
+                colorMode: 'source',
                 interactivity: true
             },
             node: {
@@ -33,7 +31,6 @@ function handleSampleDataQueryResponse(response) {
         }
     };
     var pieoptions = {
-        //title: 'My Daily Activities',
         pieHole: 0.3,
         colors: piecolors,
         pieSliceText: 'none'
@@ -80,27 +77,15 @@ function handleSampleDataQueryResponse(response) {
     var view;
     var view_sankey;
 
-    //   var chart = new google.visualization.Sankey(document.getElementById('sankey_basic'));
-    /*      var chart = new google.visualization.ChartWrapper({
-           'chartType': 'Sankey',
-           'containerId': 'sankey_basic',
-           'options': options,
-           // The pie chart will use the columns 'Name' and 'Donuts eaten'
-           // out of all the available ones.
-           'view': {
-               'columns': [1, 2, 3],
-               //                      'rows': viewRows
-           }
-       });  */
 
     var dummy = new google.visualization.ChartWrapper({
         chartType: 'Table',
         containerId: 'DummyTable',
         options: {
-            sort: 'disable' // disable sorting on the dummy table to reduce the number of event handlers spawned
+            sort: 'disable'
         },
         view: {
-           columns: [1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12] // remove all rows from the view so it doesn't waste time rendering them in HTML
+           columns: [1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12]
         }
     });
 
@@ -113,7 +98,6 @@ function handleSampleDataQueryResponse(response) {
 
             var dt = dummy.getDataTable();
             //nFilter.options.minValue = dt.getColumnRange(4).max;
-            //alert("Min: " + dt.getColumnRange(4).min);
 
             viewRows = []
             var limit = dt.getNumberOfRows();
@@ -122,29 +106,17 @@ function handleSampleDataQueryResponse(response) {
                 viewRows.push(i);
             }
 
-            // get the data for the table
-
-            // do any manipulation/aggregation here
-            // set the table's data
-
-            //var chart = new google.visualization.Sankey(document.getElementById('sankey_basic'));
-
             view = new google.visualization.DataView(dt);
             view_sankey = new google.visualization.DataView(dt);
-            //view.setRows(data.getFilteredRows([
-            //    { column: 3, minValue: 3 }
-            //]));
+
             if (dt.getNumberOfRows() > 100) {
                 view.setRows(viewRows);
                 view_sankey.setRows(viewRows);
             }
-            //view.setColumns([1, 2, 4]);
-            view.setColumns([1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
-            view_sankey.setColumns([1, 2, 4]);
 
-            //chart.setDataTable(dt);
-            //chart.view.rows = viewRows;
-            // draw the table
+            view.setColumns([1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
+            view_sankey.setColumns([1, 2, 4, 5]);
+
             chart.draw(view_sankey, options);
         });
 
@@ -156,10 +128,7 @@ function handleSampleDataQueryResponse(response) {
         for (var i = 0; i < limit; i++) {
             viewRows.push(i);
         }
-        // get the data for the table
 
-        // do any manipulation/aggregation here
-        // set the table's data
         var piechart = new google.visualization.PieChart(document.getElementById('piechart'));
         var chart = new google.visualization.Sankey(document.getElementById('sankey_basic'));
         google.visualization.events.addListener(chart, 'select', usefulHandler);
@@ -167,14 +136,6 @@ function handleSampleDataQueryResponse(response) {
             var sel = chart.getSelection()[0];
             if (sel) {
                 document.getElementById("piechart").style.visibility='visible';
-/*                 alert(' ROW ' + chart.getSelection()[0].row
-                + ' / ' + view.getValue(chart.getSelection()[0].row,0)
-                + ' / ' + view.getValue(chart.getSelection()[0].row,1)
-                + ' / ' + view.getValue(chart.getSelection()[0].row,2)
-                + ' Q ' + view.getValue(chart.getSelection()[0].row,3)
-                + ' R ' + view.getValue(chart.getSelection()[0].row,4)
-                + ' A ' + view.getValue(chart.getSelection()[0].row,8)
-                ); */
                 var piedata = new google.visualization.DataTable();
                 piedata.addColumn('string', 'Segment');
                 piedata.addColumn('number', '#users');
@@ -195,34 +156,20 @@ function handleSampleDataQueryResponse(response) {
 
         view = new google.visualization.DataView(dt);
         view_sankey = new google.visualization.DataView(dt);
-        //view.setRows(data.getFilteredRows([
-        //    { column: 3, minValue: 3 }
-        //]));
+
         if (dt.getNumberOfRows() > 100) {
             view.setRows(viewRows);
             view_sankey.setRows(viewRows);
         }
-        //view.setColumns([1, 2, 4]);
-        view.setColumns([1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
-        view_sankey.setColumns([1, 2, 4]);
 
-        //chart.setDataTable(dt);
-        //chart.view.rows = viewRows;
-        // draw the table
+        view.setColumns([1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
+        view_sankey.setColumns([1, 2, 4, 5]);
+
         chart.draw(view_sankey, options);
     });
-
-    /*    var view = new google.visualization.DataView(data);
-       view.setRows(data.getFilteredRows([
-           { column: 3, minValue: 3 }
-       ])); */
-
-
 
     dashboard.bind(programmaticFilter, dummy);
     dashboard.bind(nFilter, dummy);
     dashboard.draw(data);
 
-    //      var chart = new google.visualization.Sankey(document.getElementById('sankey_basic'));
-    //      chart.draw(data, options);
 }
